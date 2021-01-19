812cba3e80816f0e356e71d922867e2677bbf95c
fumikaobayashi <fumika.obayashi@gmail.com>
2021-01-19 19:24:11 +0900

    リセットCSS追加

--- src/resources/views/shared/head.blade.php
+++ src/resources/views/shared/head.blade.php
@@ -3,5 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="{{ __('messages.app_description') }}">
     <title>@yield('title', config('app.name'))</title>
+
+    <link href="{{ asset('css/reset.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
