89bdacd67fb0f1aaa2f66a6b4379a69d05bde4fa
fumikaobayashi <fumika.obayashi@gmail.com>
2021-01-21 20:14:37 +0900

    ランキング部分

--- src/resources/views/result/create.blade.php
+++ src/resources/views/result/create.blade.php
@@ -6,12 +6,67 @@
             <h1 class="result-title">J　U　D　G　E</h1>
         </div>
         <div class="box">
-            <p>1位：笑い飯[2.3秒]</p>
-            <p>2位：あなた[5.9秒]</p>
-            <p>3位：パンクブーブー[10.2秒]</p>
+            <p class="result-description">あなたは2位でした！</p>
+            <table class="table-top">
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>笑い飯</td>
+                        <td>2.3秒</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>ブッフェ</td>
+                        <td>5.9秒</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>パンクブーブー</td>
+                        <td>10.2秒</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table class="table-sub">
+                <tbody>
+                    <tr>
+                        <td>4</td>
+                        <td>笑い飯</td>
+                        <td>2.3秒</td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>ブッフェ</td>
+                        <td>5.9秒</td>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td>パンクブーブー</td>
+                        <td>10.2秒</td>
+                    </tr>
+                    <tr>
+                        <td>7</td>
+                        <td>笑い飯</td>
+                        <td>2.3秒</td>
+                    </tr>
+                    <tr>
+                        <td>8</td>
+                        <td>ブッフェ</td>
+                        <td>5.9秒</td>
+                    </tr>
+                    <tr>
+                        <td>9</td>
+                        <td>パンクブーブー</td>
+                        <td>10.2秒</td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>パンクブーブー</td>
+                        <td>10.2秒</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
         <div class="box">
-            <p>あなたは2位でした！</p>
             <div class="result-twitter">
                 <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false" data-size="large" data-text="結果｜{{ config('app.name') }}" data-url="https://google.co.jp">Tweet</a>
                 <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
